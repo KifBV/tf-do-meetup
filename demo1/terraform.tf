0ca8523a24821429653875a67f6ccd8be5e5c71b
Franck Ratier <ratier.franck@gmail.com>
2017-07-22 22:06:42 +0300

    add/modify slides

--- demo1/terraform.tf
+++ demo1/terraform.tf
@@ -1,43 +1,56 @@
 #############
-# VARIABLES #
-#############
-
-# Set the variable value in *.tfvars file
-# or using -var "do_token=..." CLI option
-# Do not expose your token in plain text here!
-variable "do_token" {}
-
-# Location of my public key for DO Droplets
-# It has a default value but can be overwritten if necessary
-variable "ssh_key"  { default = "~/.ssh/id_rsa_do.pub" }
-
-# We also define a default value for the Droplet size
-variable "vm_size"  { default = "512mb" }
-
-#############
 # PROVIDERS #
 #############
 
-# Configure the DigitalOcean Provider
+# A provider usually needs credentials. Also, the version can be (and should
+# be) pinned with Terraform v0.10 and above. If you remove that option from the
+# provider block and run 'terraform init', this is what Terraform will suggest.
+
 provider "digitalocean" {
   token   = "${var.do_token}"
   version = "~> 0.1"
 }
 
 #############
+# VARIABLES #
+#############
+
+# Set the token value using -var "do_token=<token>" as a CLI option (using e.g.
+# an environment variable or shell expansion).
+# Do not expose your token in plain text here!
+
+variable "do_token" { }
+
+# Location of my public key for DO Droplets. It has a default value that can be
+# overwritten in terraform.tfvars or at the CLI if necessary.
+
+variable "ssh_key"  { default = "~/.ssh/id_rsa_do.pub" }
+
+# We also declare a variable for the Droplet size. Its value is set in
+# terraform.tfvars but again, we can overwrite it at the CLI (try to add '-var
+# droplet_size="1gb"' to the terraform plan command).
+
+variable "droplet_size"  { }
+
+#############
 # RESOURCES #
 #############
 
-# Create a new Droplet in the fra1 region
+# Some options are required, others are optional (see the provider's docs). The
+# value for the 'ssh_keys' variable is the 'fingerprint' output of the
+# 'digitalocean_ssh_key' resource.
+
 resource "digitalocean_droplet" "vm1" {
   image    = "ubuntu-16-04-x64"
   name     = "demo1"
   region   = "fra1"
-  size     = "${var.vm_size}"
+  size     = "${var.droplet_size}"
   ssh_keys = ["${digitalocean_ssh_key.default.fingerprint}"]
 }
 
-# Create a new SSH key to connect to our Droplet
+# Terraform also provides useful interpolations. Here we get the value for the
+# 'public_key' variable from the content of a file.
+
 resource "digitalocean_ssh_key" "default" {
   name       = "default-key"
   public_key = "${file(var.ssh_key)}"
